bce1c39f0e9a85754c4e2e3d38ab598262ccf230
Sharnam13 <kedia.sharnam@gmail.com>
2026-04-01 20:26:02 +0530

    updated routers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from express;
+import express from "express";
 import cookieParser from "cookie-parser"; 
 import cors from "cors";
 
@@ -9,8 +9,15 @@ app.use(cors({
   
 }))
 app.use(express.json({limit: "16kb"}));
-app.se(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//routes
+import userRoute from "./routes/user.route.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRoute);
 export { app }
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../util/asyncHandler.js";
+
+const registerUser=asyncHandler(async(req,res) =>{
+  res.status(200).json({
+    message : "ok"
+  })
+})
+export {registerUser}
